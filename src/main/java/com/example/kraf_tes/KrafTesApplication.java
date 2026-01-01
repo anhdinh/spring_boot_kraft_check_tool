a79e5d5dcacee73ff610a7e5551a488f059f174b
andy <anhdt2@ngs.com.vn>
2026-01-02 01:42:50 +0700

    update

--- src/main/java/com/example/kraf_tes/KrafTesApplication.java
+++ src/main/java/com/example/kraf_tes/KrafTesApplication.java
@@ -1,15 +1,39 @@
 package com.example.kraf_tes;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka
+@Slf4j
 public class KrafTesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KrafTesApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner printEnv(Environment env) {
+		return args -> {
+			System.out.println("======= TẤT CẢ BIẾN MÔI TRƯỜNG =======");
+
+			// 1. In từ System Environment (Biến của OS/Docker/K8S)
+			System.getenv().forEach((key, value) -> {
+				System.out.println(key + " = " + value);
+			});
+
+			System.out.println("======= CẤU HÌNH SPRING ĐANG NHẬN =======");
+
+			// 2. Kiểm tra riêng các biến quan trọng của Kafka
+			System.out.println("KAFKA_SERVERS: " + env.getProperty("spring.kafka.bootstrap-servers"));
+			System.out.println("IMAGE_BUILD: " + env.getProperty("IMAGE_BUILD"));
+
+			System.out.println("=======================================");
+		};
+	}
 }
